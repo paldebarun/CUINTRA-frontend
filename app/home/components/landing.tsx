6cab32d90c5cf4f6d0e837e1738677b62c5dfd81
anshika75 <anshikathakur596@gmail.com>
2024-09-29 22:40:33 +0530

    Update landing.tsx

--- app/home/components/landing.tsx
+++ app/home/components/landing.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Image from 'next/image'
 type Props = {}
 
 export default function Landing({}: Props) {
   return (
-    <section className='w-full min-h-screen bg'>
-
+    <section className='w-full min-h-screen bg flex flex-row justify-evenly'>
+        <Image src='/landing.png' alt='landing' width={1000} height={1000} className='h-8 w-auto'/>
     </section>
   )
 }
