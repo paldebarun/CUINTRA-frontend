a0b41b7a9842b94405faf5f2f641ead805345569
anshika75 <anshikathakur596@gmail.com>
2024-09-29 22:38:44 +0530

    Update header.tsx

--- app/home/components/header.tsx
+++ app/home/components/header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 export default function Header({ }: Props) {
     return (
 
-        <header className="bg-[#151515]/[.25] backdrop-blur-[520.83px] text-white p-4 absolute top-0 left-0 w-full">
+        <header className="bg-[#151515]/[.25] backdrop-blur-[520.83px] text-white p-4 absolute top-0 left-0 w-full px-6">
             <div className="container mx-auto flex flex-wrap items-center justify-between">
                 <div className="flex items-center space-x-4">
                     <Image src="/culogo.png" alt='cu-logo' width={1000} height={1000} className='h-8 w-auto' />
@@ -14,12 +14,12 @@ export default function Header({ }: Props) {
                     <Image src="/cuactivitieslogo.png" alt='cuactivities-logo' width={1000} height={1000} className='h-8 w-auto'/>
                    
                 </div>
-                <nav className="flex items-center space-x-4">
-                    <a href="#" className="hover:underline">Home</a>
-                    <a href="#" className="hover:underline">Events</a>
-                    <a href="#" className="hover:underline">Activities</a>
-                    <a href="#" className="hover:underline">Entities</a>
-                    <button className="bg-transparent border border-white px-4 py-1 rounded hover:bg-white hover:text-red-800 transition-colors">
+                <nav className="flex items-center dm-sans-regular">
+                    <a href="#" className="hover:underline mx-3 text-lg">Home</a>
+                    <a href="#" className="hover:underline mx-3 text-lg">Events</a>
+                    <a href="#" className="hover:underline mx-3 text-lg">Activities</a>
+                    <a href="#" className="hover:underline mx-3 text-lg">Entities</a>
+                    <button className="bg-transparent border border-white px-5 py-1 rounded-full hover:bg-white hover:text-[#D91F23] transition-colors ml-20 mr-4 pb-2">
                         Login
                     </button>
                 </nav>
