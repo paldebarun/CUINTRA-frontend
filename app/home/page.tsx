3f524494ab4d82895811ac5c8174617107044ccc
anshika75 <anshikathakur596@gmail.com>
2024-09-30 23:24:09 +0530

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,22 +5,23 @@ import QuickMenu from './components/quickmenu'
 import Slider from './components/slider'
 import DiscussionForum from './components/discussionforum'
 import Ranking from './components/ranking'
+import './styles/home.css'
 
 type Props = {}
 
 export default function Home({}: Props) {
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col">
         <Header/>
         <div className="flex flex-row h-full w-full">
             <Sidebar/>
-            <div className="flex flex-col justify-center w-full h-screen overflow-scroll pt-[350px] p-6 space-y-6">
+            <div className="flex flex-col h-screen overflow-y-scroll justify-start w-full p-6 space-y-6">
                 <QuickMenu/>
                 <div className="flex flex-row space-x-6">
-                    <div className="w-[60%] rounded-[50px] overflow-hidden h-[350px]">
+                    <div className="w-[60%] rounded-[50px] shadow-lg overflow-hidden h-[370px]">
                         <Slider/>
                     </div>
-                    <div className="w-[40%] grid place-items-center">
+                    <div className="w-[40%] grid place-items-center shadow-lg rounded-[50px]">
                         <DiscussionForum/>
                     </div>
                 </div>
