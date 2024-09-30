f5f485c4c5e62a97caee1ea96f489dbcd0196cec
anshika75 <anshikathakur596@gmail.com>
2024-09-30 21:26:51 +0530

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/header'
+import Sidebar from './components/sidebar'
 
 type Props = {}
 
@@ -8,7 +9,7 @@ export default function Home({}: Props) {
     <div className="flex flex-col">
         <Header/>
         <div className="flex flex-row h-full w-full">
-
+            <Sidebar/>
         </div>
     </div>
   )
