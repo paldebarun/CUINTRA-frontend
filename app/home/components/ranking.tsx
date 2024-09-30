1d4f3e3668f4da1af1cf0d140999a01d43cf6ff6
anshika75 <anshikathakur596@gmail.com>
2024-09-30 23:06:31 +0530

    Update ranking.tsx

--- app/home/components/ranking.tsx
+++ app/home/components/ranking.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
 export default function ranking({}: Props) {
   return (
     <div className="w-full">
-        
+        <Image src='/ranking.png' alt="ranking" height={100} width={100} />
     </div>
   )
 }
