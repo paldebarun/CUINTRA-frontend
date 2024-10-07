873516d02e748110d316ef729ab9123c0bdbfa69
Debarun <paldebarun27@gmail.com>
2024-10-07 09:10:12 +0530

    use router not used error resolved

--- app/[entityName]/membershipForm/page.tsx
+++ app/[entityName]/membershipForm/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
